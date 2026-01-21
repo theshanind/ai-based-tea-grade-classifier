f205d63a1b1746f5a3aeffe725db56db8bff0bad
theshanind <indurangatheshan8@gmail.com>
2026-01-21 23:13:15 +0530

    Appjs changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path='/login' element={<PlaceholderPage pageName='login'/>}/>
+        <Route path='/register' element={<PlaceholderPage pageName='Register'/>}/>
       </Routes>
     </BrowserRouter>
 
