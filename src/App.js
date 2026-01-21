9f92f31f14fdbb57bfd87362da45d1a4f9ee247f
theshanind <indurangatheshan8@gmail.com>
2026-01-21 23:40:33 +0530

    login page implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PlaceholderPage from './placeholder_components.jsx';
 import LandingPage from './components/landing_page/landing_page';
+import LoginPage from './components/login_page/login_page.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path='/login' element={<PlaceholderPage pageName='login'/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
         <Route path='/register' element={<PlaceholderPage pageName='Register'/>}/>
       </Routes>
     </BrowserRouter>
